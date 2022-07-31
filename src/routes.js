906bc4d07deae3950d828089db61909adbe99647
brito101 <rodrigobrito101@hotmail.com>
2022-07-31 15:54:24 -0300

    ADD controllers implemntations

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const Auth = require("./middlewares/Auth")
 
 const AuthValidator = require('./validators/AuthValidator');
+const UserValidator = require('./validators/UserValidator');
 
 const AuthController = require("./controllers/AuthController")
 const UserController = require("./controllers/UserController")
@@ -13,12 +14,12 @@ router.get("/ping", (req, res) => {
   res.json({ pong: true })
 })
 
-router.post("/user/signin", AuthController.signin)
+router.post("/user/signin", AuthValidator.signin, AuthController.signin)
 router.post("/user/signup", AuthValidator.signup, AuthController.signup)
 
 router.get("/states", UserController.getStates)
 router.get("/user/me", Auth.private, UserController.info)
-router.put("/user/me", Auth.private, UserController.editAction)
+router.put('/user/me', UserValidator.editAction, Auth.private, UserController.editAction);
 
 router.get("/categories", AdController.getCategories)
 router.post("/ad/add", Auth.private, AdController.addAction)
